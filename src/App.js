abb35861fe7b2fe63945a12e9127217e34aa5f94
jessegumtow <gumtow@gmail.com>
2021-04-06 21:09:08 -0500

    set initial sort on load in useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import data from "./StudentTable.json";
 
 function App() {
+  const [myData, setData] = useState([]);
+  const [mySort, setSort] = useState(false);
+
+  useEffect(() => {
+    // Average test score sand add to data
+    let newData = data.map((data) => {
+      let avg = Math.round(
+        (data.scores.test1 + data.scores.test2 + data.scores.test3) / 3
+      );
+      return { ...data, avg };
+    });
+
+    // create an array of averages and sort in reverse order
+    let avgs = newData
+      .map((data) => {
+        return data.avg;
+      })
+      .sort()
+      .reverse();
+
+    // Add standing value based on the index of the sorted averages array
+    newData = newData.map((data, i) => {
+      let standing = avgs.indexOf(data.avg) + 1;
+      return { ...data, standing };
+    });
+
+    // Sort the new data by standing
+    mySort
+      ? newData.sort((a, b) => (a.standing > b.standing ? -1 : 1))
+      : newData.sort((a, b) => (a.standing > b.standing ? 1 : -1));
+
+    // Set myData
+    setData(newData);
+  }, [mySort]);
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <td>Student Name</td>
+            <td>Age</td>
+            <td>Test1</td>
+            <td>Test2</td>
+            <td>Test3</td>
+            <td>Avg</td>
+            <td>
+              <button onClick={() => setSort(!mySort)}>Standing</button>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {myData.map((data, i) => {
+            return (
+              <tr key={i}>
+                <td>
+                  {data.lastName}, {data.firstName}
+                </td>
+                <td>{data.age}</td>
+                <td>{data.scores.test1}</td>
+                <td>{data.scores.test2}</td>
+                <td>{data.scores.test3}</td>
+                <td>{data.avg}</td>
+                <td>{data.standing}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
